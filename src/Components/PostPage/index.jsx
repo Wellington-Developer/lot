b6f81a2f0ca42984c797331515cd61a2c5a508da
Wellington <wellingtonsantos.developer@gmail.com>
2023-12-08 01:36:14 -0300

    Feat: array 0 image

--- src/Components/PostPage/index.jsx
+++ src/Components/PostPage/index.jsx
@@ -42,7 +42,7 @@ export const PostPage = () => {
       <div className="post-page__container container">
         <div className="post-page__intro">
           <div className="post-left__intro">
-            <img src={data.imagens_relacionadas[1]} />
+            <img src={data.imagens_relacionadas[0]} />
           </div>
           <div className="post-right__intro">
             <h3 className="post-price">{Number(data.preco).toLocaleString('pt-BR', {
